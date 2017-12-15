c78f3c0217b672a443708c517dd4d19d5ac28169
Kallay_Levente <levente.kallay.ext@evosoft.com>
2017-12-15 08:52:20 +0100

    init commit2

---
+++ src/main/java/com/example/housKeeping/HousKeepingApplication.java
@@ -0,0 +1,12 @@
+package com.example.housKeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousKeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousKeepingApplication.class, args);
+	}
+}
