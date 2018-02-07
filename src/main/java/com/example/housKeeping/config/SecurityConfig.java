0b2f25ccb948107dc09e32863f116325cb4ca8a0
Kallay_Levente <levente.kallay.ext@evosoft.com>
2018-02-07 13:36:44 +0100

    set base user and password to read it from enviroment variable

--- src/main/java/com/example/housKeeping/config/SecurityConfig.java
+++ src/main/java/com/example/housKeeping/config/SecurityConfig.java
@@ -1,18 +1,19 @@
 package com.example.housKeeping.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                .withUser(System.getenv("LOGIN_USERNAME")).password(System.getenv("LOGIN_PASSWORD")).roles("USER");
     }
 }
