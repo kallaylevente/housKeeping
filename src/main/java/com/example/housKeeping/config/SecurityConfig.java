a80c3f501fbc7facd49c7becd9365b84c4d0fff4
Kallay_Levente <levente.kallay.ext@evosoft.com>
2018-02-08 15:01:43 +0100

    add custom loginpage, and logout button

--- src/main/java/com/example/housKeeping/config/SecurityConfig.java
+++ src/main/java/com/example/housKeeping/config/SecurityConfig.java
@@ -2,14 +2,13 @@ package com.example.housKeeping.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -19,6 +18,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser(System.getenv("LOGIN_USERNAME")).password(System.getenv("LOGIN_PASSWORD")).roles("ADMIN");
 
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
     }
 
 }
