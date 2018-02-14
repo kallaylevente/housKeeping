4ad5e1610d01611f0cdd26a00315a4f1bb56074e
Kallay_Levente <levente.kallay.ext@evosoft.com>
2018-02-14 07:42:05 +0100

    make enviroment variable for owner password on startup

--- src/main/java/com/example/housKeeping/config/StartupConfig.java
+++ src/main/java/com/example/housKeeping/config/StartupConfig.java
@@ -1,6 +1,8 @@
 package com.example.housKeeping.config;
 
 
+import static java.lang.System.getenv;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -15,14 +17,14 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class StartupConfig implements ApplicationListener<ApplicationReadyEvent>{
+public class StartupConfig implements ApplicationListener<ApplicationReadyEvent> {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRolesRepository userRolesRepository;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        userRepository.save(HouseKeepingUser.builder().username("kallaylevente").password(passwordEncoder.encode("password")).enable(true).build());
-        userRolesRepository.save(UserRoles.builder().username("kallaylevente").role("ADMIN").build());
+        userRepository.save(HouseKeepingUser.builder().username(getenv("LOGIN_USERNAME")).password(passwordEncoder.encode(getenv("LOGIN_PASSWORD"))).enable(true).build());
+        userRolesRepository.save(UserRoles.builder().username(getenv("LOGIN_USERNAME")).role("ADMIN").build());
     }
 }
